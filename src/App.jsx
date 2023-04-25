af72dd793a326bc35f61970b8323448b8f3ba8d4
Leticia Negreiros <leticia.negreiros@outlook.com>
2023-04-25 11:05:37 -0300

    CSS Desktop finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -14,39 +14,46 @@ function App() {
           <div className="about">
             <h1 className="about-title">Sobre Rick and Morty</h1>
             <p className="synopsis">
-              Rick Sanchez é um cientista genial e alcoólatra que foi morar com a
-              família de sua filha Beth, uma cirurgiã cardíaca de equinos. Ele
+              Rick Sanchez é um cientista genial e alcoólatra que foi morar com
+              a família de sua filha Beth, uma cirurgiã cardíaca de equinos. Ele
               divide seu tempo entre desenvolver projetos altamente tecnológicos
-              em seu laboratório (garagem da casa de Beth) e levar seu neto de 14
-              anos Morty em aventuras perigosas e surreais pelo Multiverso.
+              em seu laboratório (garagem da casa de Beth) e levar seu neto de
+              14 anos Morty em aventuras perigosas e surreais pelo Multiverso.
               Combinados com tensões preexistentes dentro da família, esses
-              eventos causam ao sensível Morty muito angústia em casa e na escola.
+              eventos causam ao sensível Morty muito angústia em casa e na
+              escola.
             </p>
           </div>
           <div className="discover">
             <h2 className="discover-title">Descubra personagens</h2>
             <p className="click-the-button-paragraph">
-              Clique no portal para descobrir um personagem aleatório!
+              Clique no portal ao lado para descobrir um personagem aleatório!
             </p>
           </div>
-          <div className="button-container">
-            <p className="btn-paragraph">Get Schwifty!</p>
-            <button>
-              <img className="btn-portal" src="src/assets/portal.png" alt="" />
-            </button>
-          </div>
+          <div className="button-container"></div>
         </div>
+        <button>
+          <img className="btn-portal" src="src/assets/portal.png" alt="" />
+        </button>
         <div className="character-result-container">
-          <h2>Jerry Smith</h2>
-          <img
+          <h2 className="character-name">Jerry Smith</h2>
+          <img className="character-img"
             src="https://rickandmortyapi.com/api/character/avatar/5.jpeg"
             alt="Jerry Smith"
             width="250"
           />
-          <p>Gênero: Masculino</p>
-          <p>Espécie: Humano</p>
-          <p>Origem: Planeta Terra</p>
-          <p>Número de episódios em que aparece: xx</p>
+          <p>
+            <span className="bold">Gênero:</span> Masculino
+          </p>
+          <p>
+            <span className="bold">Espécie:</span> Humano
+          </p>
+          <p>
+            <span className="bold">Origem:</span> Planeta Terra
+          </p>
+          <p>
+            <span className="bold">Número de episódios em que aparece:</span> xx
+          </p>
         </div>
       </main>
     </div>
